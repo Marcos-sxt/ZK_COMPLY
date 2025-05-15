c9ed5eab622065a9b68482e49afd63aeae3cd1a8
Raphael Dantas <raphael.dantas@sonda.com>
2025-05-15 10:35:33 -0300

    fix: verify the number of substances on /substance and change deprecated time from verify endpoint

--- backend/routes/substance.py
+++ backend/routes/substance.py
@@ -2,17 +2,18 @@ from fastapi import APIRouter, HTTPException
 from schemas.base import SubstanceRequest, SubstanceResponse
 from schemas.verify import VerifyRequest, VerifyResponse
 from services.substance_service import generate_commitment_hash, simulate_verification
-from datetime import datetime
+from datetime import datetime, timezone
 
 router = APIRouter()
 
 @router.post("/substance", response_model=SubstanceResponse)
 def create_substance(data: SubstanceRequest):
+  if not 2 <= data.composition <= 30: raise HTTPException(status_code=400, detail="composition must be between 2 and 30")
   hash_commitment = generate_commitment_hash(data.name, data.composition)
   return {
     "name": data.name,
     "hash": hash_commitment,
-    "timestamp": datetime.utcnow()
+    "timestamp": datetime.now(timezone.utc)
   }
 
 @router.post("/verify", response_model=VerifyResponse)
